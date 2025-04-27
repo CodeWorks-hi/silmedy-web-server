e2cacd59385c8abbab434d1438d144c7c77269ed
nhchoi2 <choi1278@gmail.com>
2025-04-27 23:56:22 +0900

    Update doctor_service.py

--- app/services/doctor_service.py
+++ app/services/doctor_service.py
@@ -1,3 +1,4 @@
+
 from firebase_admin import firestore
 
 def get_all_doctors():
@@ -6,26 +7,18 @@ def get_all_doctors():
     docs = collection_doctors.stream()
     return [doc.to_dict() for doc in docs]
 
-# 의사 문서 삭제
 def delete_doctor_by_license(license_number):
     db = firestore.client()
     collection_doctors = db.collection("doctors")
     docs = collection_doctors.where("license_number", "==", license_number).stream()
-
     for doc in docs:
-        doc_id = doc.id
-        print(f"Deleting doctor doc ID = {doc_id}")
         doc.reference.delete()
 
-# 의사 정보 업데이트
 def update_doctor_by_license(license_number, payload):
     db = firestore.client()
     collection_doctors = db.collection("doctors")
     docs = collection_doctors.where("license_number", "==", license_number).stream()
-
     for doc in docs:
-        doc_id = doc.id
-        print(f"Updating doctor doc ID = {doc_id} with {payload}")
         doc.reference.update(payload)
 
 def register_doctor(payload):
@@ -33,20 +26,15 @@ def register_doctor(payload):
     collection_doctors = db.collection("doctors")
     collection_doctors.add(payload)
 
-# 의사로그인
 def find_doctor_by_credentials(payload):
     db = firestore.client()
+    collection_doctors = db.collection("doctors")
     license_number = payload.get("license_number")
     password = payload.get("password")
     if not (license_number and password):
         return None
-
-    doc_ref = db.collection("doctors").document(license_number)
-    snapshot = doc_ref.get()
-    if snapshot.exists:
-        data = snapshot.to_dict()
-        if data.get("password") == password:
-            data["documentId"] = snapshot.id
-            return data
-
+    docs = collection_doctors.where("license_number", "==", license_number).where("password", "==", password).stream()
+    for doc in docs:
+        return doc.to_dict()
     return None
+
