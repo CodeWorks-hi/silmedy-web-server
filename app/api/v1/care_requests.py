79e81176446c0dde4c4722e449b3bad4471b69ac
nhchoi2 <choi1278@gmail.com>
2025-04-29 11:15:48 +0900

    Update care_requests.py

--- app/api/v1/care_requests.py
+++ app/api/v1/care_requests.py
@@ -10,8 +10,8 @@ router = APIRouter()
 
 @router.get("/care-requests")
 async def list_care_requests():
-    return {"care_requests": get_all_care_requests()}
+    return get_all_care_requests()
 
 @router.get("/care-requests/waiting")
 async def list_waiting_care_requests():
-    return {"waiting_list": get_waiting_care_requests()}
+    return get_waiting_care_requests()
